e3e7578d50e8cd217a50d4db4d0bab825c841049
Vadim Macagon <vadim.macagon@gmail.com>
2014-07-27 15:40:27 +0700

    Add a few types like FVector, FQuat, FTransform to the generated .csproj
    These types currently don't have auto-generated C# wrappers, so for the
    time being they are defined as simple value types in the C# wrapper
    project.

---
+++ Engine/Plugins/Script/ScriptGeneratorPlugin/Resources/WrapperProjectTemplate/UE4Structs.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Klawr.UnrealEngine
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct FVector2D
+    {
+        public float X;
+        public float Y;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct FVector
+    {
+        public float X;
+        public float Y;
+        public float Z;
+    }
+
+    // TODO: The FVector4 class is 16-byte aligned, but the mirror FVector4 USTRUCT is not,
+    //       which one of those is this struct actually going to correspond to? And does it matter?
+    [StructLayout(LayoutKind.Sequential)]
+    public struct FVector4
+    {
+        public float X;
+        public float Y;
+        public float Z;
+        public float W;
+    }
+
+    // TODO: Check the alignment is correct
+    [StructLayout(LayoutKind.Sequential)]
+    public struct FQuat
+    {
+        public float X;
+        public float Y;
+        public float Z;
+        public float W;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct FTransform
+    {
+        public FQuat Rotation;
+        public FVector Translation;
+        public FVector Scale3D;
+    }
+
+    // TODO: Check the alignment is correct
+    [StructLayout(LayoutKind.Sequential)]
+    public struct FColor
+    {
+        public byte B, G, R, A;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct FLinearColor
+    {
+        public float B, G, R, A;
+    }
+}
