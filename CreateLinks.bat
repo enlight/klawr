a44c37e6d56aa53f6e44a855c745efcafc573f26
Vadim Macagon <vadim.macagon@gmail.com>
2014-08-07 21:57:53 +0700

    Add a batch file to create some directory junctions and symbolic links
    Due to a shared directory structure I can't just checkout the Klawr
    source into a checkout of Unreal Engine source (since they both use git).
    To avoid tedious copy/pasting between the two I've created some
    directory junctions and symbolic links.

---
+++ CreateLinks.bat
@@ -0,0 +1,13 @@
+:: Engine source is in a git repo, Klawr source is in a different git repo, no simple way to put the Klawr checkout inside Engine checkout due to a shared directory structure,
+:: so create a couple of directory junctions and a few symbolic links in the Engine source that link to the Klawr source.
+set ScriptGeneratorLinkDir=C:\Projects\UE4\UnrealEngine\Engine\Plugins\Script\ScriptGeneratorPlugin
+set ScriptGeneratorTargetDir=C:\Projects\UE4\klawr\Engine\Plugins\Script\ScriptGeneratorPlugin
+mklink /J C:\Projects\UE4\UnrealEngine\Engine\Source\ThirdParty\Klawr C:\Projects\UE4\klawr\Engine\Source\ThirdParty\Klawr
+mklink /J %ScriptGeneratorLinkDir%\Resources\WrapperProjectTemplate %ScriptGeneratorTargetDir%\Resources\WrapperProjectTemplate
+mklink %ScriptGeneratorLinkDir%\Source\ScriptGeneratorPlugin\Private\KlawrCodeFormatter.h %ScriptGeneratorTargetDir%\Source\ScriptGeneratorPlugin\Private\KlawrCodeFormatter.h
+mklink %ScriptGeneratorLinkDir%\Source\ScriptGeneratorPlugin\Private\KlawrCodeGenerator.cpp %ScriptGeneratorTargetDir%\Source\Private\ScriptGeneratorPlugin\KlawrCodeGenerator.cpp
+mklink %ScriptGeneratorLinkDir%\Source\ScriptGeneratorPlugin\Private\KlawrCodeGenerator.h %ScriptGeneratorTargetDir%\Source\ScriptGeneratorPlugin\Private\KlawrCodeGenerator.h
+mklink %ScriptGeneratorLinkDir%\Source\ScriptGeneratorPlugin\Private\pugiconfig.hpp %ScriptGeneratorTargetDir%\Source\ScriptGeneratorPlugin\Private\pugiconfig.hpp
+mklink %ScriptGeneratorLinkDir%\Source\ScriptGeneratorPlugin\Private\pugixml.cpp %ScriptGeneratorTargetDir%\Source\ScriptGeneratorPlugin\Private\pugixml.cpp
+mklink %ScriptGeneratorLinkDir%\Source\ScriptGeneratorPlugin\Private\pugixml.hpp %ScriptGeneratorTargetDir%\Source\ScriptGeneratorPlugin\Private\pugixml.hpp
+pause
