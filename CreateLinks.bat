72ed88b0e110f4b0c5ed50d7a2583511503a4323
Vadim Macagon <vadim.macagon@gmail.com>
2014-09-07 16:22:36 +0700

    Add proper build instructions to README

--- CreateLinks.bat
+++ CreateLinks.bat
@@ -5,9 +5,9 @@ set BATCH_FILE_LOCATION=%~dp0
 set ENGINE_SOURCE_LOCATION=C:\Projects\UE4\UnrealEngine\
 @echo on
 
-mklink /J %ENGINE_SOURCE_LOCATION%Engine\Source\ThirdParty\Klawr %BATCH_FILE_LOCATION%Engine\Source\ThirdParty\Klawr
-mklink /J %ENGINE_SOURCE_LOCATION%Engine\Plugins\Klawr\KlawrCodeGeneratorPlugin %BATCH_FILE_LOCATION%Engine\Plugins\Klawr\KlawrCodeGeneratorPlugin
-mklink /J %ENGINE_SOURCE_LOCATION%Engine\Plugins\Klawr\KlawrRuntimePlugin %BATCH_FILE_LOCATION%Engine\Plugins\Klawr\KlawrRuntimePlugin
-mklink /J %ENGINE_SOURCE_LOCATION%Engine\Plugins\Klawr\KlawrEditorPlugin %BATCH_FILE_LOCATION%Engine\Plugins\Klawr\KlawrEditorPlugin
+mklink /J "%ENGINE_SOURCE_LOCATION%Engine\Source\ThirdParty\Klawr" "%BATCH_FILE_LOCATION%Engine\Source\ThirdParty\Klawr"
+mklink /J "%ENGINE_SOURCE_LOCATION%Engine\Plugins\Klawr\KlawrCodeGeneratorPlugin" "%BATCH_FILE_LOCATION%Engine\Plugins\Klawr\KlawrCodeGeneratorPlugin"
+mklink /J "%ENGINE_SOURCE_LOCATION%Engine\Plugins\Klawr\KlawrRuntimePlugin" "%BATCH_FILE_LOCATION%Engine\Plugins\Klawr\KlawrRuntimePlugin"
+mklink /J "%ENGINE_SOURCE_LOCATION%Engine\Plugins\Klawr\KlawrEditorPlugin" "%BATCH_FILE_LOCATION%Engine\Plugins\Klawr\KlawrEditorPlugin"
 
 pause
